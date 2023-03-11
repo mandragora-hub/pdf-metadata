f2f97f2bb512381a9dcc44f7e99452901c011ab9
mandragora-hub <royer01gomez@gmail.comn>
2023-03-11 12:45:03 -0400

    Completing process

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
-import { PdfReader } from "pdfreader";
-import fs from "fs";
-import readingTime from "reading-time"
-import { PDFDocument } from 'pdf-lib';
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
-function getRawTextContent(pdfBuffer) {
-    const promise = new Promise((resolve, reject) => {
+import readingTime from "reading-time";
+import { getRawTextContent, getMetadataAndPages } from "./lib.js";
 
-        const pdfReader = new PdfReader();
-        const textArray = []
-        pdfReader.parseBuffer(pdfBuffer, (err, item) => {
-            if (err) reject("error:", err);
-            else if (!item) {
-                const text = textArray.join(" ")
-                return resolve(text);
-            }
-            else if (item.text) {
-                textArray.push(item.text)
-            };
-        });
-    })
+const LITTERARUM_BASEURL = process.env.LITTERARUM_URL_API || "http://localhost:3000";
+const LITTERARUM_TOKEN_API = process.env.LITTERARUM_TOKEN_API;
 
-    return promise
+const myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${LITTERARUM_TOKEN_API}`);
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+function downloadAndProcess(bookId) {
+  const promise = new Promise((resolve, reject) => {
+    fetch(`${LITTERARUM_BASEURL}/api/v1/books/${bookId}/download`, requestOptions)
+      .then((response) => response.arrayBuffer())
+      .then(async (arrayBuffer) => {
+        const pdfBuffer = Buffer.from(arrayBuffer);
+        const text = await getRawTextContent(pdfBuffer);
+        const { pages, metadata } = await getMetadataAndPages(pdfBuffer);
+        const stats = readingTime(text);
+        resolve({ pages, metadata, stats });
+      })
+      .catch((error) => reject(error));
+  });
+  return promise;
 }
 
-function getMetadata(pdfBuffer) {
-    const promise = new Promise((resolve, reject) => {
-        PDFDocument.load(pdfBuffer, { updateMetadata: false }).then((pdfDoc) => {
-            const pages = pdfDoc.getPages()
-            resolve({
-                pages: pages.length,
-                title: pdfDoc.getTitle(),
-                author: pdfDoc.getAuthor(),
-                subject: pdfDoc.getSubject(),
-                creator: pdfDoc.getCreator(),
-                keywords: pdfDoc.getKeywords(),
-                producer: pdfDoc.getProducer(),
-                creationDate: pdfDoc.getCreationDate(),
-                modificationDate: pdfDoc.getModificationDate(),
-            })
+function save(bookId, data) {
+  const putHeaders = new Headers();
+  putHeaders.append("Authorization", `Bearer ${LITTERARUM_TOKEN_API}`);
+  putHeaders.append("Content-Type", "application/json");
 
-        })
-    })
-    return promise;
+  const { pages, metadata, stats } = data;
+  const raw = JSON.stringify({ pages: pages, wordCount: stats.words, readTime: stats.time, metadata });
+
+  const requestOptions = {
+    method: "PUT",
+    headers: putHeaders,
+    body: raw,
+  };
+
+  const promise = new Promise((resolve, reject) => {
+    fetch(`${LITTERARUM_BASEURL}/api/v1/books/${bookId}`, requestOptions)
+      .then((response) => response.json())
+      .then((results) => resolve(results))
+      .catch((error) => reject(error));
+  });
+  return promise;
 }
 
-
-fs.readFile("test/sample.pdf", async (err, pdfBuffer) => {
-    if (err) { return console.log(err) }
-    const text = await getRawTextContent(pdfBuffer).catch((err) => { console.error(err) });
-    const metadata = await getMetadata(pdfBuffer).catch((err) => { console.error() })
-    const stats = readingTime(text);
-    console.log({ metadata, stats })
-})
+fetch(`${LITTERARUM_BASEURL}/api/v1/books`, requestOptions)
+  .then((response) => response.json())
+  .then(async (result) => {
+    for (const book of result.data) {
+      try {
+        const bookId = book._id;
+        const data = await downloadAndProcess(bookId);
+        const t = await save(bookId, data);
+        console.log(t);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })
+  .catch((error) => console.error("fetch error", error));
