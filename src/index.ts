1a7e93432d597284683b86a8e0197bd91df6b145
mandragora-hub <royer01gomez@gmail.comn>
2023-07-19 21:00:57 -0400

    Installing tsc and jest

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// import readingTime from "reading-time";
+// import { getRawTextContent, getMetadataAndPages } from "./lib.js";
+
+export default async function extractPDFMetadata(arrayBuffer: ArrayBuffer) {
+  // const pdfBuffer = Buffer.from(arrayBuffer);
+  // const text = await getRawTextContent(pdfBuffer);
+  // const { pages, metadata } = await getMetadataAndPages(pdfBuffer);
+  // const stats = readingTime(text);
+
+  // return { pages: pages, wordCount: stats.words, readTime: stats.time, metadata };
+
+  return "asdfsadf"
+}
