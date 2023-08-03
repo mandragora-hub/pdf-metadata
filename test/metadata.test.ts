388472785fe207c4ca9b9586c9e044e53ced6720
mandragora-hub <royer01gomez@gmail.comn>
2023-08-02 20:37:34 -0400

    ⚗️ Update test

--- test/metadata.test.ts
+++ test/metadata.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, test } from "@jest/globals";
 import { loadDocument } from "../src/index";
 
+const MAX_TIMEOUT = 40000;
+
 describe("Get info of module", () => {
-  test("Load and get info document", async () => {
-    const url = "https://litterarum.onrender.com/api/v1/files/el-hombre-que-calculaba-malba-tahan.pdf";
-    const doc = await loadDocument(url);
-    const info = await doc.getInfo();
-    expect(info).toBeDefined();
-  }, 70000);
+  test(
+    "Load and get info document",
+    async () => {
+      const url = "https://litterarum.onrender.com/api/v1/files/el-hombre-que-calculaba-malba-tahan.pdf";
+      const doc = await loadDocument(url);
+      const info = await doc.getInfo();
+      expect(info).toMatchSnapshot();
+    },
+    MAX_TIMEOUT,
+  );
 });
